2adb6f806b8431ff02686f9459144af19c9035a1
hakam <atassi.hakam@gmail.com>
2024-05-20 03:51:03 +0300

    gave up on github actions for now. new testing

--- cocotb/functional_tests/Frontend/rename/reorder_renamed_outputs_dut.py
+++ cocotb/functional_tests/Frontend/rename/reorder_renamed_outputs_dut.py
@@ -12,17 +12,17 @@ class reorder_renamed_outputs_dut:
         self.dut.io_renamed_valid_2.value = valid_bits[2]
         self.dut.io_renamed_valid_3.value = valid_bits[3]
 
-        self.dut.io_renamed_values_0 = data[0]
-        self.dut.io_renamed_values_1 = data[1]
-        self.dut.io_renamed_values_2 = data[2]
-        self.dut.io_renamed_values_3 = data[3]
+        self.dut.io_renamed_values_0.value = data[0]
+        self.dut.io_renamed_values_1.value = data[1]
+        self.dut.io_renamed_values_2.value = data[2]
+        self.dut.io_renamed_values_3.value = data[3]
 
     def get_output(self):
         sorted_data = [0]*4
 
-        sorted_data[0] = int(self.dut.io_renamed_values_sorted_0)
-        sorted_data[1] = int(self.dut.io_renamed_values_sorted_1)
-        sorted_data[2] = int(self.dut.io_renamed_values_sorted_2)
-        sorted_data[3] = int(self.dut.io_renamed_values_sorted_3)
+        sorted_data[0] = int(self.dut.io_renamed_values_sorted_0.value)
+        sorted_data[1] = int(self.dut.io_renamed_values_sorted_1.value)
+        sorted_data[2] = int(self.dut.io_renamed_values_sorted_2.value)
+        sorted_data[3] = int(self.dut.io_renamed_values_sorted_3.value)
 
         return sorted_data
