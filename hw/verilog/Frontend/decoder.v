902036e85363c3329d204e0c3709d3b2379be357
hakam <atassi.hakam@gmail.com>
2024-05-25 19:32:41 +0300

    LSQ left

--- hw/verilog/Frontend/decoder.v
+++ hw/verilog/Frontend/decoder.v
@@ -14,15 +14,26 @@ module decoder(
   output [5:0]  io_decoded_instruction_ROB_index,
   output [4:0]  io_decoded_instruction_instructionType,
   output [1:0]  io_decoded_instruction_portID,
-  output        io_decoded_instruction_SUBTRACT,
-                io_decoded_instruction_MULTIPLY
+                io_decoded_instruction_RS_type,
+  output        io_decoded_instruction_needs_ALU,
+                io_decoded_instruction_needs_branch_unit,
+                io_decoded_instruction_SUBTRACT,
+                io_decoded_instruction_MULTIPLY,
+                io_decoded_instruction_IMMEDIATE
 );
 
-  wire       _needs_ALU_T = io_instruction_instruction[6:2] == 5'hC;
+  wire       _is_INT_T = io_instruction_instruction[6:2] == 5'hC;
+  wire       IMMEDIATE = io_instruction_instruction[6:2] == 5'h4;
+  wire       _is_INT_T_3 = io_instruction_instruction[6:2] == 5'h18;
+  wire       _is_INT_T_5 = io_instruction_instruction[6:2] == 5'h1B;
+  wire       _is_INT_T_7 = io_instruction_instruction[6:2] == 5'h19;
+  wire       needs_branch_unit = _is_INT_T_3 | _is_INT_T_5 | _is_INT_T_7;
   wire       needs_ALU =
-    _needs_ALU_T
+    _is_INT_T
     & (io_instruction_instruction[27] | io_instruction_instruction[31:25] == 7'h0)
-    | io_instruction_instruction[6:2] == 5'h4;
+    | IMMEDIATE;
+  wire       _is_MEM_T_1 = io_instruction_instruction[6:2] == 5'h8;
+  wire       _is_MEM_T = io_instruction_instruction[6:2] == 5'h0;
   reg  [1:0] ALU_port;
   always @(posedge clock) begin
     if (reset)
@@ -65,19 +76,21 @@ module decoder(
   assign io_decoded_instruction_portID =
     needs_ALU
       ? ALU_port
-      : _needs_ALU_T
+      : _is_INT_T
         & (io_instruction_instruction[14:12] == 3'h4
            | io_instruction_instruction[14:12] == 3'h5
            | io_instruction_instruction[14:12] == 3'h6
            | (&(io_instruction_instruction[14:12]))) & io_instruction_instruction[25]
           ? 2'h1
-          : io_instruction_instruction[6:2] == 5'h18
-            | io_instruction_instruction[6:2] == 5'h1B
-            | io_instruction_instruction[6:2] == 5'h19
-              ? 2'h0
-              : {2{io_instruction_instruction[6:2] == 5'h8
-                     | io_instruction_instruction[6:2] == 5'h0}};
-  assign io_decoded_instruction_SUBTRACT = _needs_ALU_T & io_instruction_instruction[27];
-  assign io_decoded_instruction_MULTIPLY = _needs_ALU_T & io_instruction_instruction[25];
+          : needs_branch_unit ? 2'h0 : {2{_is_MEM_T_1 | _is_MEM_T}};
+  assign io_decoded_instruction_RS_type =
+    _is_INT_T | IMMEDIATE | _is_INT_T_3 | _is_INT_T_5 | _is_INT_T_7
+      ? 2'h0
+      : _is_MEM_T | _is_MEM_T_1 ? 2'h1 : 2'h2;
+  assign io_decoded_instruction_needs_ALU = needs_ALU;
+  assign io_decoded_instruction_needs_branch_unit = needs_branch_unit;
+  assign io_decoded_instruction_SUBTRACT = _is_INT_T & io_instruction_instruction[27];
+  assign io_decoded_instruction_MULTIPLY = _is_INT_T & io_instruction_instruction[25];
+  assign io_decoded_instruction_IMMEDIATE = IMMEDIATE;
 endmodule
 
