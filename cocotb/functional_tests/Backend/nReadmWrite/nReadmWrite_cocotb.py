9b153a03b7994413526e10faf8b1c66d9d7bb90d
hakam <atassi.hakam@gmail.com>
2024-05-25 20:14:03 +0300

    PRF fuzz tests

--- cocotb/functional_tests/Backend/nReadmWrite/nReadmWrite_cocotb.py
+++ cocotb/functional_tests/Backend/nReadmWrite/nReadmWrite_cocotb.py
@@ -103,3 +103,80 @@ async def simple_writes_4_wide_8_wide(dut):
         assert outputs[6] == i + 6
         assert outputs[7] == i + 7
         await ReadWrite()
+
+@cocotb.test()
+async def fuzz_writes_4_wide_8_wide(dut):
+    random.seed(0x42)
+
+    await cocotb.start(generateClock(dut)) 
+
+    await reset(dut)
+    await RisingEdge(dut.clock)
+
+    dut = nReadmWrite(dut)
+
+    mem=[0]*64
+
+    for _ in range(10000):
+        wr_en0   = random.randint(0,1)
+        wr_addr0 = random.randint(0, 63)
+        wr_data0 = random.randint(0, (1<<32)-1)
+
+        wr_en1   = random.randint(0,1)
+        wr_addr1 = random.randint(0, 63)
+        wr_data1 = random.randint(0, (1<<32)-1)
+
+        wr_en2   = random.randint(0,1)
+        wr_addr2 = random.randint(0, 63)
+        wr_data2 = random.randint(0, (1<<32)-1)
+
+        wr_en3   = random.randint(0,1)
+        wr_addr3 = random.randint(0, 63)
+        wr_data3 = random.randint(0, (1<<32)-1)
+
+        addr_in     =[wr_addr0, wr_addr1, wr_addr2, wr_addr3]
+        wr_en       =[wr_en0, wr_en1, wr_en2, wr_en3]
+        wr_data_in  =[wr_data0, wr_data1, wr_data2, wr_data3]
+
+        dut.write(addr_in=addr_in, wr_en=wr_en, data_in=wr_data_in)
+
+        if(wr_en0): mem[wr_addr0] = wr_data0; print(f"mem[{wr_addr0}] = {wr_data0}")
+        if(wr_en1): mem[wr_addr1] = wr_data1; print(f"mem[{wr_addr1}] = {wr_data1}")
+        if(wr_en2): mem[wr_addr2] = wr_data2; print(f"mem[{wr_addr2}] = {wr_data2}")
+        if(wr_en3): mem[wr_addr3] = wr_data3; print(f"mem[{wr_addr3}] = {wr_data3}")
+
+        await RisingEdge(dut.clock())
+
+        dut.write()
+
+        rd_addr0 = random.randint(0, 63)
+        rd_addr1 = random.randint(0, 63)
+        rd_addr2 = random.randint(0, 63)
+        rd_addr3 = random.randint(0, 63)
+        rd_addr4 = random.randint(0, 63)
+        rd_addr5 = random.randint(0, 63)
+        rd_addr6 = random.randint(0, 63)
+        rd_addr7 = random.randint(0, 63)
+
+        rd_addr_in=[rd_addr0, rd_addr1, rd_addr2, rd_addr3, rd_addr4, rd_addr5, rd_addr6, rd_addr7]
+
+        dut.read(addr_in=rd_addr_in)
+    
+        await RisingEdge(dut.clock())
+        
+        await ReadOnly()
+
+        outputs = dut.get_output()
+
+        assert outputs[0] == mem[rd_addr0]
+        assert outputs[1] == mem[rd_addr1]
+        assert outputs[2] == mem[rd_addr2]
+        assert outputs[3] == mem[rd_addr3]
+        assert outputs[4] == mem[rd_addr4]
+        assert outputs[5] == mem[rd_addr5]
+        assert outputs[6] == mem[rd_addr6]
+        assert outputs[7] == mem[rd_addr7]
+
+        await ReadWrite()
+
+
