34ccab17c2d1f7cbdc817cd9224bf7a350ede62c
Hakam Atassi <atassi.hakam@gmail.com>
2024-08-12 11:38:27 -0400

    some uvm stuff

---
+++ tb/UVM/source_tools.sh
@@ -0,0 +1,22 @@
+## SETUP CMC TOOLS
+
+# Setup virtuoso
+source /CMC/scripts/cadence.ic06.18.320.csh
+
+# Setup XCelium
+source /CMC/scripts/cadence.xceliummain23.03.004.csh
+
+# Setup Jasper Gold
+
+# Setup Genus
+source /CMC/scripts/cadence.genus.21.17.000.csh
+
+# Setup vsim
+source /CMC/scripts/siemens.questasim.2022.1_2.csh
+
+
+
+## SETUP dirs #
+#set UVM_HOME /CMC/tools/siemens/questasim_2022.1_2/questasim/uvm-1.1d
+#set QUESTA_HOME /CMC/tools/siemens/questasim_2022.1_2/questasim
+#set UVM_SRC /CMC/tools/siemens/questasim_2022.1_2/questasim/verilog_src/uvm-1.1d/src
