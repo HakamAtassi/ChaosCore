fecdd1cef630f2007d782ab846c7b54a0291c81f
hakam atassi <atassi.hakam@gmail.com>
2024-09-17 19:51:50 -0400

    instret seems to work but stalls with -O1

--- binaries/C/instret.c
+++ binaries/C/instret.c
@@ -1,6 +1,10 @@
 #include "utils/utils.c"
 #include <stdint.h>
 
+
+#define cycle  0xc00  // CSR for low 32 bits of the instruction retirement counter
+#define cycleh 0xc80  // CSR for high 32 bits of the instruction retirement counter
+
 #define instret  0xc02  // CSR for low 32 bits of the instruction retirement counter
 #define instreth 0xc82  // CSR for high 32 bits of the instruction retirement counter
 
@@ -17,11 +21,25 @@ int read_instret() {
     return instret_low;
 }
 
+int read_cycle() {
+    int cycle_low;
+
+    // Read the low 32 bits of instret
+    asm volatile("csrr %0, %1" : "=r"(cycle_low) : "i"(cycle));
+
+    // Read the high 32 bits of instret
+    //asm volatile("csrr %0, %1" : "=r"(instret_high) : "i"(instreth));
+
+    // Combine the high and low bits into a 64-bit result
+    return cycle_low;
+}
+
 int main() {
 
     printf("Running\n");
     // Read initial instret  
     int initial_instret = read_instret();
+    int initial_cycle = read_cycle();
 
     // Perform a few instructions
     int a = 0;
@@ -34,15 +52,15 @@ int main() {
 
     // Read the new instret
     int new_instret = read_instret();
+    int new_cycle = read_cycle();
 
     // Print the difference in instret (instructions retired)
     printf("Initial instret: %0d\n", initial_instret);
     printf("New instret: %0d\n", new_instret);
     printf("Instructions retired: %0d\n", new_instret - initial_instret);
+    printf("cycles: %0d\n", new_cycle - initial_cycle);
 
-
-    printf("%0d \n", a);
-
+    printf("a: %0d \n", a);
 
     return 0;
 }
