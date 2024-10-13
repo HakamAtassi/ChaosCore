dac4ab8ae4f3c100bf040f5112d5ce94d0e7dc3b
alisher3 <rakhimoa@mcmaster.ca>
2024-10-13 17:58:40 -0400

    removed python venv

--- venv/lib/python3.12/site-packages/cocotb_bus/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright cocotb contributors
-# Licensed under the Revised BSD License, see LICENSE for details.
-# SPDX-License-Identifier: BSD-3-Clause
-
-from ._version import __version__  # noqa: F401
--- venv/lib/python3.12/site-packages/cocotb_utils/__init__.py
+++
@@ -1,1 +0,0 @@
-from .cocotb_utils import *
