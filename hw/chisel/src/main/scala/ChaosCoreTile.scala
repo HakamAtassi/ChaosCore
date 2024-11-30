265fd7f4b611d09b2eb9bb16c6528ee245dc80e9
hakam atassi <atassi.hakam@gmail.com>
2024-11-30 10:01:40 -0500

    added chipyard configs

--- hw/chisel/src/main/scala/ChaosCoreTile.scala
+++ hw/chisel/src/main/scala/ChaosCoreTile.scala
@@ -215,4 +215,18 @@ class ChaosCoreTileModuleImp(outer: ChaosCoreTile) extends BaseTileModuleImp(out
   // INIT the core
   // connect the I$ and D$
 
+  val core = Module(new ChaosCore(CoreParameters()))
+
+
+        //val frontend_memory_request             =   Decoupled(new frontend_memory_request(coreParameters))
+        //val frontend_memory_response            =   Flipped(Decoupled(new fetch_packet(coreParameters)))
+
+        /////////////////////////////
+        //// D$ BACKEND MEM ACCESS //
+        /////////////////////////////
+        //val backend_memory_request              =   Decoupled(new backend_memory_request(coreParameters))
+        //val backend_memory_response             =   Flipped(Decoupled(new backend_memory_response(coreParameters)))
+
+
+
 }
---
+++ hw/chisel/src/main/scala/Configs.scala
@@ -0,0 +1,51 @@
+//******************************************************************************
+// Copyright (c) 2019 - 2019, The Regents of the University of California (Regents).
+// All Rights Reserved. See LICENSE and LICENSE.SiFive for license details.
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// ChaosCore Tile Wrapper
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+package ChaosCore
+
+import chisel3._
+import chisel3.util._
+import chisel3.experimental.{IntParam, StringParam}
+
+import scala.collection.mutable.{ListBuffer}
+
+import org.chipsalliance.cde.config._
+import freechips.rocketchip.subsystem._
+import freechips.rocketchip.devices.tilelink._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.prci._
+import freechips.rocketchip.rocket._
+import freechips.rocketchip.subsystem.{RocketCrossingParams}
+import freechips.rocketchip.tilelink._
+import freechips.rocketchip.interrupts._
+import freechips.rocketchip.util._
+import freechips.rocketchip.tile._
+import freechips.rocketchip.amba.axi4._
+
+
+
+class WithNChaosCore(n: Int = 1) extends Config((site, here, up) => {
+  case TilesLocated(InSubsystem) => {
+    // Calculate the next available hart ID (since hart ID cannot be duplicated)
+    val prev = up(TilesLocated(InSubsystem), site)
+    val idOffset = up(NumTiles)
+    // Create TileAttachParams for every core to be instantiated
+    (0 until n).map { i =>
+      ChaosCoreAttachParams(
+        tileParams = ChaosCoreTileParams(tileId = i + idOffset),
+        crossingParams = RocketCrossingParams()
+      )
+    } ++ prev
+  }
+  // Configurate # of bytes in one memory / IO transaction. For RV64, one load/store instruction can transfer 8 bytes at most.
+  case SystemBusKey => up(SystemBusKey, site).copy(beatBytes = 4)
+  case NumTiles => up(NumTiles) + n
+})
